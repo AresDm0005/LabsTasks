fc8e50d7e6f16bcbbd1fdc27ae0efe44a25c8b70
Arseniy Dmitriev <44650732+AresDm0005@users.noreply.github.com>
2020-02-07 12:13:53 +0500

    part 1 added

--- Lab9/Lab9/Program.cs
+++ Lab9/Lab9/Program.cs
@@ -10,6 +10,19 @@ namespace Lab9
     {
         static void Main(string[] args)
         {
+            int h1 = int.Parse(Console.ReadLine()), m1 = int.Parse(Console.ReadLine());
+            int h2= int.Parse(Console.ReadLine()), m2 = int.Parse(Console.ReadLine());
+
+            Time t1 = new Time(h1, m1);
+            Time t2 = new Time(h2, m2);
+
+
+            Time t3 = Time.DeductTime(t1, t2);
+            Time t4 = t1.DeductTime(t2);
+
+            Console.WriteLine(t3.ToString());
+            Console.WriteLine(t4.ToString());
+
         }
     }
 }
---
+++ Lab9/Lab9/Time.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9
+{
+    class Time
+    {
+        private int hours;
+        private int minutes;
+        private static int count = 0;
+
+        public int Hours
+        {
+            get { return hours; }
+            private set
+            {
+                if (value < 0)
+                {
+                    hours = 0;
+                    minutes = 0;
+                }
+                else hours = value;
+            }
+        }
+
+        public int Minutes
+        {
+            get { return minutes; }
+            private set
+            {
+                if (value < 0)
+                {
+                    value = -value;
+                    if(value < hours * 60)
+                    {
+                        hours -= (value - 1) / 60 + 1;
+                        minutes = 60 - value % 60;
+                    }
+                    else
+                    {
+                        minutes = 0;
+                        hours = 0;
+                    }
+                }
+                else if (value >= 60)
+                {
+                    minutes = value;
+                    hours += minutes / 60;
+                    minutes %= 60;
+                }
+                else minutes = value;
+            }
+        }
+
+        public int Count
+        {
+            get { return count; }
+            private set { count = value; }
+        }
+
+        public Time() : this(0, 0) { }
+
+        public Time(int hours, int minutes)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Count++;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{Hours} ч {Minutes} мин.");
+        }
+
+        public static Time DeductTime(Time t1, Time t2)
+        {
+            Time tmp = new Time();
+            tmp.Hours = t1.Hours - t2.Hours;
+            tmp.Minutes = t1.Minutes - t2.Minutes;
+            return tmp;
+        }
+
+        public Time DeductTime(Time t)
+        {
+            Time tmp = new Time(this.Hours -= t.Hours, this.Minutes - t.Minutes);
+            return tmp;
+        }
+
+        public override string ToString()
+        {
+            return $"{Hours} ч {Minutes} мин.";
+        }
+    }
+}
