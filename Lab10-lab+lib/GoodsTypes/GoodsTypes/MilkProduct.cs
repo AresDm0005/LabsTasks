bd277f9aa7231cf8513b2f0f02e6f2c1651fffde
Arseniy Dmitriev <44650732+AresDm0005@users.noreply.github.com>
2020-02-26 19:31:13 +0500

    lib project added

---
+++ Lab10-lab+lib/GoodsTypes/GoodsTypes/MilkProduct.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace GoodsTypes
+{
+    public class MilkProduct : FoodProduct
+    {
+        private string type;
+        private double weight;
+
+        public string Type
+        {
+            get { return type; }
+            protected set { type = value; }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            protected set
+            {
+                if (weight < 0) weight = 0;
+                else weight = value;
+            }
+        }
+
+        public MilkProduct() : base()
+        {
+            Type = "Undetermined";
+            Weight = 1.0;
+        }
+
+        public MilkProduct(string title, string manuf) : base(title, manuf)
+        {
+            Type = "Undetermined";
+            Weight = 1.0;
+        }
+
+        public MilkProduct(string title, string manuf, string type, double weight) : base(title, manuf)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        public MilkProduct(string title, string manuf, int price, int quantity, string type, double weight) : base(title, manuf, price, quantity)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        public MilkProduct(string title, string manuf, int price, int quantity, DateTime date, int lifeSpan, string type, double weight) : base(title, manuf, price, quantity, date, lifeSpan)
+        {
+            Type = type;
+            Weight = weight;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine($"Молочные продукты:\n" +
+                $"Название: {Title}, производитель: {Manufacturer}\n" +
+                $"Произведено: {Manufactured}, срок годности: {StorageLife}ч\n" +
+                $"Тип: {Type}, вес: {Weight} кг\n" +
+                $"Стоимость: {Price}р., Количество на складе: {Quantity} шт.\n");
+        }
+
+        public override string ToString()
+        {
+            return $"Молочка: {Title}, {Manufacturer}, {Manufactured}, {StorageLife}ч.,\n{Type}, {Weight}, {Price}p., {Quantity}";
+        }
+
+    }
+}
---
+++ Lab10-lab+lib/GoodsTypes/GoodsTypes/Toys.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace GoodsTypes
+{
+    public class Toys : Goods
+    {
+        private int ageRestriction;
+        private string type;
+
+        public int AgeRestriction
+        {
+            get { return ageRestriction; }
+            protected set
+            {
+                if (value < 0) ageRestriction = 0;
+                else ageRestriction = value;
+            }
+
+        }
+
+        public string Type
+        {
+            get { return type; }
+            protected set { type = value; }
+        }
+
+        public Toys() : base()
+        {
+            AgeRestriction = 3;
+            Type = "NotDetermined";
+        }
+
+        public Toys(string title, string manuf) : base(title, manuf)
+        {
+            AgeRestriction = 3;
+            Type = "NotDetermined";
+        }
+
+        public Toys(string title, string manuf, int age, string type) : base(title, manuf)
+        {
+            AgeRestriction = age;
+            Type = type;
+        }
+
+        public Toys(string title, string manuf, int price, int quantity, int age, string type) : base(title, manuf, price, quantity)
+        {
+            AgeRestriction = age;
+            Type = type;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine($"Игрушка:\n" +
+                $"Название: {Title}, производитель: {Manufacturer}\n" +
+                $"Тип: {Type}, {AgeRestriction}+\n" +
+                $"Стоимость: {Price}р., Количество на складе: {Quantity} шт.\n");
+        }
+
+        public override string ToString()
+        {
+            return $"Игрушка: {Title}, {Manufacturer}, {Type}, {AgeRestriction}+, {Price}р., {Quantity} шт.";
+        }
+    }
+}
